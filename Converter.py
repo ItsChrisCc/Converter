a030e16aa4ee1bb45af5f143d092aa5e2d8e62da
ItsChrisCc <106062690+ItsChrisCc@users.noreply.github.com>
2023-09-20 11:19:22 -0400

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,10 @@
+#🌡Celsius and Farenheit Converter
+celsius = 200
+farenheit = 200 #⬇️Celsius to Farenheit⬇️
+print(f"{celsius*1.8+32} F°") #⬇️Farenheit to Celsius⬇️
+print(f"{(farenheit-32)*5/9} C°")
+
+#Hz -> RPM⬇️
+Hz = 60
+print(f"{Hz*60} rpm") #RPM -> Hz⬇️
+rpm = 3200 #print(f"{rpm/60} Hz")
